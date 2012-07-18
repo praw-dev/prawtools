eabbde2f08c5091b84a940a0d2bf57c7f31f49a5
Bryce Boe <bbzbryce@gmail.com>
2012-07-17 21:47:54 -0700

    Added prawtools package and configure setup.py accordingly.

--- setup.py
+++ setup.py
@@ -2,25 +2,32 @@ import os
 import re
 from setuptools import setup
 
+PACKAGE_NAME = 'prawtools'
+
 HERE = os.path.abspath(os.path.dirname(__file__))
-MODULE_FILE = os.path.join(HERE, 'subreddit_stats.py')
+INIT = open(os.path.join(HERE, PACKAGE_NAME, '__init__.py')).read()
 README = open(os.path.join(HERE, 'README.md')).read()
-VERSION = re.search("__version__ = '([^']+)'",
-                    open(MODULE_FILE).read()).group(1)
-
-NAME = 'subreddit_stats'
+VERSION = re.search("__version__ = '([^']+)'", INIT).group(1)
 
 
-setup(name=NAME,
+setup(name=PACKAGE_NAME,
       version=VERSION,
       author='Bryce Boe',
       author_email='bbzbryce@gmail.com',
-      url='https://github.com/praw-dev/subreddit_stats',
-      description=('A tool to calculate various submission and comment '
-                   'statistics on reddit communities.'),
+      url='https://github.com/praw-dev/prawtools',
+      description=('A collection of utilities that utilize the reddit API.'),
       long_description=README,
-      keywords='reddit subreddit statistics',
-      classifiers=['Programming Language :: Python'],
+      keywords='reddit mod moderator subreddit statistics tools',
+      classifiers=['Environment :: Console',
+                   'Intended Audience :: Developers',
+                   'Natural Language :: English',
+                   'Operating System :: OS Independent',
+                   'Programming Language :: Python :: 2.6',
+                   'Programming Language :: Python :: 2.7',
+                   'Programming Language :: Python :: 3.2',
+                   'Topic :: Utilities'],
       install_requires=['praw'],
-      py_modules=[NAME],
-      entry_points={'console_scripts': ['{0} = {0}:main'.format(NAME)]})
+      packages=[PACKAGE_NAME],
+      entry_points={'console_scripts':
+                        ['subreddit_stats = prawtools.stats:main',
+                         'modutils = prawtools.mod:main']})
