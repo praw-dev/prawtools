6eb835401d9c92243d34cf4b8c50c797758b1526
Bryce Boe <bbzbryce@gmail.com>
2014-01-16 10:38:38 -0800

    Make wheelable, use with around open files in setup.py. Version bump.

--- setup.py
+++ setup.py
@@ -4,10 +4,12 @@ from setuptools import setup
 
 PACKAGE_NAME = 'prawtools'
 
+
 HERE = os.path.abspath(os.path.dirname(__file__))
-INIT = open(os.path.join(HERE, PACKAGE_NAME, '__init__.py')).read()
-README = open(os.path.join(HERE, 'README.md')).read()
-VERSION = re.search("__version__ = '([^']+)'", INIT).group(1)
+with open(os.path.join(HERE, 'README.md')) as fp:
+    README = fp.read()
+with open(os.path.join(HERE, PACKAGE_NAME, '__init__.py')) as fp:
+    VERSION = re.search("__version__ = '([^']+)'", fp.read()).group(1)
 
 
 setup(name=PACKAGE_NAME,
@@ -31,7 +33,7 @@ setup(name=PACKAGE_NAME,
                    'Programming Language :: Python :: 3.2',
                    'Programming Language :: Python :: 3.3',
                    'Topic :: Utilities'],
-      install_requires=['praw>=2.1.2'],
+      install_requires=['praw>=2.1.2', 'update_checker>=0.7'],
       packages=[PACKAGE_NAME],
       entry_points={'console_scripts':
                         ['modutils = prawtools.mod:main',
