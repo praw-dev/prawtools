e3d9a9a2b4c85f065238f813975f642c5448571a
Bryce Boe <bbzbryce@gmail.com>
2016-06-19 12:37:14 -0700

    Update setup.py, .gitignore and lint/pre_push script.

--- setup.py
+++ setup.py
@@ -1,26 +1,23 @@
-import os
+"""prawtools setup.py."""
+
 import re
+from codecs import open
+from os import path
 from setuptools import setup
 
+
 PACKAGE_NAME = 'prawtools'
-
-
-HERE = os.path.abspath(os.path.dirname(__file__))
-with open(os.path.join(HERE, 'README.md')) as fp:
+HERE = path.abspath(path.dirname(__file__))
+with open(path.join(HERE, 'README.md'), encoding='utf-8') as fp:
     README = fp.read()
-with open(os.path.join(HERE, PACKAGE_NAME, '__init__.py')) as fp:
+with open(path.join(HERE, PACKAGE_NAME, '__init__.py'),
+          encoding='utf-8') as fp:
     VERSION = re.search("__version__ = '([^']+)'", fp.read()).group(1)
 
 
 setup(name=PACKAGE_NAME,
-      version=VERSION,
       author='Bryce Boe',
       author_email='bbzbryce@gmail.com',
-      license='Simplified BSD License',
-      url='https://github.com/praw-dev/prawtools',
-      description=('A collection of utilities that utilize the reddit API.'),
-      long_description=README,
-      keywords='reddit mod moderator subreddit statistics tools',
       classifiers=['Environment :: Console',
                    'Intended Audience :: Developers',
                    'License :: OSI Approved :: BSD License',
@@ -32,10 +29,19 @@ setup(name=PACKAGE_NAME,
                    'Programming Language :: Python :: 3.4',
                    'Programming Language :: Python :: 3.5',
                    'Topic :: Utilities'],
-      install_requires=['praw >=4.0.0b4, <5', 'update_checker >=0.7, <1'],
-      packages=[PACKAGE_NAME],
+      description='A collection of utilities that utilize the reddit API.',
       entry_points={
           'console_scripts': ['modutils = prawtools.mod:main',
                               'reddit_alert = prawtools.alert:main',
                               'subreddit_stats = prawtools.stats:main']},
-      test_suite='tests')
+      install_requires=['praw >=4.0.0b4, <5', 'update_checker >=0.7, <1'],
+      keywords='reddit mod moderator subreddit statistics tools',
+      license='Simplified BSD License',
+      long_description=README,
+      packages=[PACKAGE_NAME],
+      test_suite='tests',
+      tests_require=['betamax >=0.5.1, <0.6',
+                     'betamax-serializers >=0.1.1, <0.2',
+                     'mock ==1.0.1'],
+      url='https://github.com/praw-dev/prawtools',
+      version=VERSION)
