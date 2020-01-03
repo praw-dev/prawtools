075ab5e79814af67c5feb0434e63d5fd78f87ecf
Bryce Boe <bbzbryce@gmail.com>
2020-01-03 08:35:18 -0800

    Run black on project

--- prawtools/helpers.py
+++ prawtools/helpers.py
@@ -6,24 +6,31 @@ from update_checker import update_check
 from . import __version__
 
 
-AGENT = 'prawtools/{}'.format(__version__)
+AGENT = "prawtools/{}".format(__version__)
 
 
 def arg_parser(*args, **kwargs):
     """Return a parser with common options used in the prawtools commands."""
     msg = {
-        'site': 'The site to connect to defined in your praw.ini file.',
-        'update': 'Prevent the checking for prawtools package updates.'}
+        "site": "The site to connect to defined in your praw.ini file.",
+        "update": "Prevent the checking for prawtools package updates.",
+    }
 
-    kwargs['version'] = 'BBoe\'s PRAWtools {}'.format(__version__)
+    kwargs["version"] = "BBoe's PRAWtools {}".format(__version__)
     parser = OptionParser(*args, **kwargs)
-    parser.add_option('-v', '--verbose', action='count', default=0,
-                      help='Increase the verbosity by 1 each time')
-    parser.add_option('-U', '--disable-update-check', action='store_true',
-                      help=msg['update'])
+    parser.add_option(
+        "-v",
+        "--verbose",
+        action="count",
+        default=0,
+        help="Increase the verbosity by 1 each time",
+    )
+    parser.add_option(
+        "-U", "--disable-update-check", action="store_true", help=msg["update"]
+    )
 
-    group = OptionGroup(parser, 'Site/Authentication options')
-    group.add_option('-S', '--site', help=msg['site'])
+    group = OptionGroup(parser, "Site/Authentication options")
+    group.add_option("-S", "--site", help=msg["site"])
     parser.add_option_group(group)
 
     return parser
@@ -32,4 +39,4 @@ def arg_parser(*args, **kwargs):
 def check_for_updates(options):
     """Check for package updates."""
     if not options.disable_update_check:  # Check for updates
-        update_check('prawtools', __version__)
+        update_check("prawtools", __version__)
--- setup.py
+++ setup.py
@@ -6,42 +6,50 @@ from os import path
 from setuptools import setup
 
 
-PACKAGE_NAME = 'prawtools'
+PACKAGE_NAME = "prawtools"
 HERE = path.abspath(path.dirname(__file__))
-with open(path.join(HERE, 'README.md'), encoding='utf-8') as fp:
+with open(path.join(HERE, "README.md"), encoding="utf-8") as fp:
     README = fp.read()
-with open(path.join(HERE, PACKAGE_NAME, '__init__.py'),
-          encoding='utf-8') as fp:
-    VERSION = re.search("__version__ = '([^']+)'", fp.read()).group(1)
+with open(path.join(HERE, PACKAGE_NAME, "__init__.py"), encoding="utf-8") as fp:
+    VERSION = re.search('__version__ = "([^"]+)', fp.read()).group(1)
 
 
-setup(name=PACKAGE_NAME,
-      author='Bryce Boe',
-      author_email='bbzbryce@gmail.com',
-      classifiers=['Environment :: Console',
-                   'Intended Audience :: Developers',
-                   'License :: OSI Approved :: BSD License',
-                   'Natural Language :: English',
-                   'Operating System :: OS Independent',
-                   'Programming Language :: Python :: 2.7',
-                   'Programming Language :: Python :: 3',
-                   'Programming Language :: Python :: 3.4',
-                   'Programming Language :: Python :: 3.5',
-                   'Programming Language :: Python :: 3.6',
-                   'Topic :: Utilities'],
-      description='A collection of utilities that utilize the reddit API.',
-      entry_points={
-          'console_scripts': ['modutils = prawtools.mod:main',
-                              'reddit_alert = prawtools.alert:main',
-                              'subreddit_stats = prawtools.stats:main']},
-      install_requires=['praw >=4.0.0, <7', 'six >=1, <2'],
-      keywords='reddit mod moderator subreddit statistics tools',
-      license='Simplified BSD License',
-      long_description=README,
-      packages=[PACKAGE_NAME],
-      test_suite='tests',
-      tests_require=['betamax >=0.7.1, <0.8',
-                     'betamax-serializers >=0.2.0, <0.3',
-                     'mock ==1.0.1'],
-      url='https://github.com/praw-dev/prawtools',
-      version=VERSION)
+setup(
+    name=PACKAGE_NAME,
+    author="Bryce Boe",
+    author_email="bbzbryce@gmail.com",
+    classifiers=[
+        "Environment :: Console",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: BSD License",
+        "Natural Language :: English",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Topic :: Utilities",
+    ],
+    description="A collection of utilities that utilize the reddit API.",
+    entry_points={
+        "console_scripts": [
+            "modutils = prawtools.mod:main",
+            "reddit_alert = prawtools.alert:main",
+            "subreddit_stats = prawtools.stats:main",
+        ]
+    },
+    install_requires=["praw >=4.0.0, <7", "six >=1, <2"],
+    keywords="reddit mod moderator subreddit statistics tools",
+    license="Simplified BSD License",
+    long_description=README,
+    packages=[PACKAGE_NAME],
+    test_suite="tests",
+    tests_require=[
+        "betamax >=0.7.1, <0.8",
+        "betamax-serializers >=0.2.0, <0.3",
+        "mock ==1.0.1",
+    ],
+    url="https://github.com/praw-dev/prawtools",
+    version=VERSION,
+)
